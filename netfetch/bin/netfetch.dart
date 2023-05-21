837aadc0393408eeec4b26d5a195f3c6c6653999
Sunny-Mason Sunny-Mason <20211060163@mail.ynu.edu.cn>
2023-05-21 16:31:06 +0000

    SAVE

---
+++ netfetch/bin/netfetch.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) async{
+  final httpPackageUrl = Uri.https('raw.githubusercontent.com', '/Sunny-Mason/my_netfetch/main/my_data.json');
+  final httpPackageInfo = await http.read(httpPackageUrl);
+  print(httpPackageInfo);
+}
